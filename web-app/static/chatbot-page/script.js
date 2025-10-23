7fa3237eea65a4d1c90c34cca69e68dbb4f2f839
Marvin Cristhian <marvincristhian07.contato@gmail.com>
2025-10-22 22:05:15 -0300

    Incrementando algoritmo do chatbot script.js
    Continuidade ao desenvolvimento do algoritmo javascript de chatbot com API do Gemini 2.5 Flash.
    
    Ainda em desenvolvimento.

--- web-app/static/chatbot-page/script.js
+++ web-app/static/chatbot-page/script.js
@@ -19,30 +19,55 @@ document.addEventListener('DOMContentLoaded', () => {
      
       setTimeout(updateProgress, stepInterval);
     } else {
-      // Quando o carregamento chega a 100%
-      // 1. Esconde o preloader com uma transição suave
       loaderWrapper.classList.add('hidden');
      
-      // 2. Mostra o conteúdo da página com uma transição suave
       document.body.classList.add('loaded');
     }
   }
 
-  // Inicia a animação quando o DOM estiver completamente carregado
   updateProgress();
  
   // ----------------------------------------------------- //
   const chatsContainer = document.querySelector(".chats-container");
   const promptForm = document.querySelector(".prompt-form");
   const promptInput = document.querySelector(".prompt-input");
+  
+  const API_KEY = "";
+  const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
  
   let userMessage = "";
+  const chatHistory = [];
  
   const createMsgElement = (content, ...classNames) => {
     const div = document.createElement("div");
     div.classList.add("message", ...classNames);
     div.innerHTML = content;
     return div;
+  }
+  
+  const generateResponse = async (botMsgHTML) => {
+    const textElement = botMsgHTML.querySelector(".message-text");
+    
+    chatHistory.push({
+      role: "user",
+      parts: [{ text: userMessage }]
+    })
+    
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ contents: chatHistory })
+      });
+      
+      const data = await response.json();
+      if(!response.ok) throw new Error(data.error.message);
+      
+      const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim();
+      textElement.textContent = responseText;
+    } catch (error) {
+      console.log(error);
+    }
   }
  
   const handleFormSubmit = (e) => {
@@ -62,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const botMsgHTML = `<img src="https://raw.githubusercontent.com/FoodYze/MyGeli/refs/heads/main/App/build_Heitor/build/assets/frame1/geli_icon.png" alt="Imagem da Geli" class="avatar">Just a sec...<p class="message-text"></p>`;
       const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
       chatsContainer.appendChild(botMsgDiv);
-      generateResponse();
+      generateResponse(botMsgDiv);
     }, 600);
   }
  
